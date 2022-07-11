bfca1b69109d5edd42b5b598b73aa5ab683ad069
sande265 <sandeshsingh265@gmail.com>
2022-07-11 20:03:18 +0545

    Initialize at this point of time

--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -4,5 +4,4 @@ export const useCors: any = (req: Request, res: Response, next: NextFunction) =>
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-    
 }
